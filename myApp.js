67964449fbb2156a8fb4b82e1798ad597605e361
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-10-16 15:42:16 +0200

    third commit, root path response as template.html

--- myApp.js
+++ myApp.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var app = express();
-console.log('Hello World')
-app.get("/", (req, res) => {
-    res.send("Hello Express")
+console.log('Hello World') // log hello world
+/*
+app.get("/", (req, res) => { // return string when user visits root
+    res.send('Hello Express')
 })
-
+*/
+app.get("/", (req, res) => { // return template when user visits root
+    res.sendFile(__dirname + 'views/index.html')
+})
 
 
 
