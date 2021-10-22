035a8a6f2602cf68740e0cd5d71ce4a34e3a7c24
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-10-22 07:47:43 +0200

    fifth.4 commit, moved logic outside res.json

--- myApp.js
+++ myApp.js
@@ -3,22 +3,24 @@ var app = express();
 console.log('Hello World') // log hello world
 /*
 app.get("/", (req, res) => { // return string when user visits root
-    res.send('Hello Express')
+	res.send('Hello Express')
 })
 */
 app.use('/public', express.static(__dirname + '/public')) // path for styles.css in public folder. Express.static is same as {{ load static }}?
 
 app.get("/", (req, res) => { // return template when user visits root
-    res.sendFile(__dirname + '/views/index.html')
+	res.sendFile(__dirname + '/views/index.html')
 })
 
 app.get("/json", (req, res) => { // API response by GET from "/json" route.
 
-    let result = 'Hello json'
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        result = result.toUpperCase()
-    }
-    res.json({"message": result})
+	let result = ''
+	if (process.env.MESSAGE_STYLE === "uppercase") {
+		result = result.toUpperCase()
+	} else {
+		result = 'Hello not json'
+	}
+	res.json({"message": result})
 })
 
 
