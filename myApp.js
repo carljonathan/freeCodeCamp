344c52d4d5926bd096374bc62cfd8b21be3d75ed
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-11-05 07:05:32 +0100

    fifth.7 commit, debugging

--- myApp.js
+++ myApp.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
+require('dotenv').config();
 var express = require('express');
 var app = express();
 console.log('Hello World') // log hello world
 /*
 app.get("/", (req, res) => { // return string when user visits root
-	res.send('Hello Express')
+    res.send('Hello Express')
 })
 */
 app.use('/public', express.static(__dirname + '/public')) // path for styles.css in public folder. Express.static is same as {{ load static }}?
 
 app.get("/", (req, res) => { // return template when user visits root
-	res.sendFile(__dirname + '/views/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 })
 
 app.get("/json", (req, res) => { // API response by GET from "/json" route.
-
-	if (process.env.MESSAGE_STYLE === "uppercase") {
-		res.json({"message": "Hello json".toUpperCase()})
-	} else {
-		res.json({"message": "Hello not json"})
-	}
+    const result = 'hello json'.toUpperCase()
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+        res.json({"message": result})
+    } else {
+        res.json({'message': 'Hello not json'})
+    }
 })
 
 
